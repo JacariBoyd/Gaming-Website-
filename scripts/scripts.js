fd68e1c15a0d1a97f00abc3909e487ec87bac420
Terrance IvyBrown <boxman06@yahoo.com>
2021-09-13 00:48:59 -0500

    Master (#5)
    * First commit
    
    * added html file
    
    * boilerplate testcode started
    
    * sample 2nd commit
    
    * working event listener
    
    * working save
    
    * Moving Slider
    
    * updated slider configuration
    
    * final slider configuration
    
    * Sidebar nav nay?yay?
    
    * fixed css console error
    
    * updated slider bar
    
    * merged html welcome page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,50 +1,20 @@
 
-// const carouselSlide = document.querySelector(".carousel-slide");
-// const carouselImages = document.querySelectorAll(".carousel-slide img");
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 5000);
 
-// //buttons
-// const prevBtn = document.querySelector("#prevBtn");
-// const nextBtn = document.querySelector("#nextBtn");
+$(".menu-btn").click(function () {
+  $(".side-bar").addClass("active");
+  $;
+});
 
-// let counter = 1;
-// const size = carouselImages[0].clientWidth;
-
-// carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-
-//Button Listeners
-// nextBtn.addEventListener("click", () => {
-//   event.preventDefault();
-//   carouselSlide.style.transition = "transform 0.4s ease-in-out";
-//   counter++;
-//   carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-// });
-
-// prevBtn.addEventListener("click", () => {
-//   event.preventDefault();
-//   carouselSlide.style.transition = "transform 0.4s ease-in-out";
-//   counter--;
-//   carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-// });
-
-//for loops
-
-// function loopInt() {
-//   let totalLoop = 0;
-//   for (let i = 0; i < carouselSlide.length.onClick(prevBtn); i++) {
-//     totalLoop += console.log("Image" + totalLoop[i]);
-//   }
-//   document.getElementsById("eventClick").onclick = function () {
-//     loopSlide.totalLoop[i];
-//   };
-// }
-
-// function MakeSlidePrev() {
-//   console.log("Start");
-//   document.getElementById("prevBtn").addEventListener("click", function cb() {
-//     console.log("Callback");
-//   });
-//   console.log("End");
-// }
-
+$(".close-btn").click(function () {
+  $(".side-bar").removeClass("active");
+});
 
 
